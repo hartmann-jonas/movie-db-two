f72057a2aa39600f2fb1dc0646205929d77d3676
Jonas Hartmann <80170229+hartmann-jonas@users.noreply.github.com>
2023-02-09 16:43:56 +0100

    Add vercel audience analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import '../global.css';
 	import Nav from '../components/Nav.svelte';
+
+	import { dev } from '$app/environment';
+  	import { inject } from '@vercel/analytics';
+
+  	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
